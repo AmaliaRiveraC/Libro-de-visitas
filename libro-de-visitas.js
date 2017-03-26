24dce557bf21bc007b6fe5aa99d5279ffae24b03
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-03-26 12:08:27 -0600

    vista previa del comentario

--- libro-de-visitas.js
+++ libro-de-visitas.js
@@ -1,19 +1,21 @@
-function design()
-{ 
-      document.getElementById("ingresarComentario").style.backgroundColor="#e9e9e9"; 
-      document.getElementById(men).style.color="#d32e12";
-}
 
-function duplicarComentario() {
+	
 
-var ingresarComentario = document.getElementById("ingresarComentario").value;
-var comentarioEnTiempoReal = document.getElementById("comentarioEnTiempoReal").value;
-document.getElementById("ingresarComentario").value = document.getElementById("comentarioEnTiempoReal").value;
+function duplicar() {
+
+var vistaPrevia = document.getElementById("ingresarComentario").value;
+document.getElementById("comentarioEnTiempoReal").innerHTML = vistaPrevia;
 }
 
 function publicarComentario() {
+	//Se comienza guardando el valor que el usurio ingrese en la <textarea> en una variable par así poderlo reproducir y publicar.
 	var comentario = document.getElementById("ingresarComentario").value;
-	var comentarioAPublicar = document.createElement("span");
+	//Esta línea indica que desea crea un elemento igual a nuestro parrafo elegidoS
+	var comentarioAPublicar = document.createElement("li");
+	 comentarioAPublicar.id = comentario;
 	comentarioAPublicar.innerHTML = comentario;
 	document.getElementById("comentariosPublicados").appendChild(comentarioAPublicar);
+	
 }
+ 
+ 
