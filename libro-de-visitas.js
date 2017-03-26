eb829f7f8ed865e5951917aa85a4f7d2d58ba0f1
AmaliaRiveraC <amaliariveracastillejos@gmail.com>
2017-03-26 12:33:23 -0600

    se agregó style.color en vista previa

--- libro-de-visitas.js
+++ libro-de-visitas.js
@@ -7,9 +7,23 @@ var vistaPrevia = document.getElementById("ingresarComentario").value;
 document.getElementById("comentarioEnTiempoReal").innerHTML = vistaPrevia;
 }
 
+function colorDeTexto(){
+	var eleccionUsuario = prompt("Ingresa el código hexadecimal del color de texto que desees:  ");
+	eleccionUsuario = "#" + eleccionUsuario;
+	document.getElementById("comentarioEnTiempoReal").style.color = eleccionUsuario;
+}
+
+
+
+
+function colorDeFondo(){
+	var eleccionUsuario = prompt("Ingresa el código hexadecimal del color de fondo que desees:  ");
+	eleccionUsuario = "#" + eleccionUsuario;
+	document.getElementById("comentarioEnTiempoReal").style.backgroundColor = eleccionUsuario;
+}
 function publicarComentario() {
 	//Se comienza guardando el valor que el usurio ingrese en la <textarea> en una variable par así poderlo reproducir y publicar.
-	var comentario = document.getElementById("ingresarComentario").value;
+	var comentario = document.getElementById("comentarioEnTiempoReal").innerHTML;
 	//Esta línea indica que desea crea un elemento igual a nuestro parrafo elegidoS
 	var comentarioAPublicar = document.createElement("li");
 	 comentarioAPublicar.id = comentario;
